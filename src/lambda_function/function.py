c2ee855d59001df2c17759cd337e08f8507a69d9
Gabriel Wortmann <wortmanngabriel@gmail.com>
2019-04-13 18:01:32 -0500

    Create function.py

---
+++ src/lambda_function/function.py
@@ -0,0 +1,23 @@
+import boto3
+import json
+import logging.config
+
+from uuid import uuid4
+
+CLIENT = boto3.client('stepfunctions')
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def handler(event, context):
+    response = CLIENT.client.start_execution(
+        stateMachineArn=event['stateMachineArn'],
+        name=event.get('name', str(uuid4())),
+        input=json.dumps(
+            event.get('input', {}), 
+            seperators=(',', ':')
+        )
+    )
+    response['startDate'] = response['startDate'].isoformat()
+    return response
